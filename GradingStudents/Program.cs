77a13d8f6646e110cda4a53038f5cf7d6b788f0c
pablobarrientos-cba <49885476+pablobarrientos-cba@users.noreply.github.com>
2019-09-16 10:42:23 -0300

    [Grading Students Challenge Solution]

---
+++ GradingStudents/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingStudents
+{
+    internal static class Result
+    {
+
+        /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+        public static IEnumerable<int> GradingStudents(IEnumerable<int> grades)
+        {
+            var result = new List<int>();
+            
+            foreach (var grade in grades)
+            {
+                var finalGrade = grade;
+                if (grade >= 38 && grade % 5 >= 3)
+                {
+                    finalGrade = grade + 5 - grade % 5;
+                }
+                result.Add(finalGrade);
+            }
+
+            return result;
+        }
+    }
+
+    public static class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var gradesCount = Convert.ToInt32(Console.ReadLine());
+
+            var grades = new List<int>();
+
+            for (var i = 0; i < gradesCount; i++)
+            {
+                var gradesItem = Convert.ToInt32(Console.ReadLine());
+                grades.Add(gradesItem);
+            }
+
+            var result = Result.GradingStudents(grades);
+            foreach (var grade in result)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ My Hacker Rank Solutions on C# language.
 - Mini Max Sum Challenge Solution
 - Birthday Cake Candles Challenge Solution
 - Time Conversion Challenge Solution
+- Grading Students Challenge Solution
